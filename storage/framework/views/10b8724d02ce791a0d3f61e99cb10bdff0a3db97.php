6b9dfaacdc5ccce593c82d5f993321dde7e6a2bb
Mohamed Magdy <mohamedmagdy2891@gmail.com>
2022-02-25 20:46:41 +0200

    Revert "Second Commit V0.1"
    This reverts commit a52ee2d68511ecaabe3f55fbe85496d73a16bc5f.

--- storage/framework/views/10b8724d02ce791a0d3f61e99cb10bdff0a3db97.php
+++
@@ -1,9 +0,0 @@
-<!-- /.content-wrapper -->
-<footer class="main-footer">
-    <strong>كل حقوق المليكة لـــ HighPharma &copy;.</strong>
-
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0 Beta
-    </div>
-</footer>
-<?php /**PATH C:\Mohamed\programes\Newfolder\Newfolder\HighPharma\resources\views/admin/layouts/footer.blade.php ENDPATH**/ ?>
